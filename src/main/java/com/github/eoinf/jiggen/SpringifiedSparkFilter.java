5b71e88c320bda96e4eaeec3ad226123dbf5d5e1
Eoin Flanagan <outterest@hotmail.com>
2018-01-14 16:37:06 +0100

    Application now launches successfully with spring and hibernate integration

--- src/main/java/com/github/eoinf/jiggen/SpringifiedSparkFilter.java
+++ src/main/java/com/github/eoinf/jiggen/SpringifiedSparkFilter.java
@@ -1,18 +1,22 @@
 package com.github.eoinf.jiggen;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.stereotype.Repository;
 import spark.servlet.SparkApplication;
 import spark.servlet.SparkFilter;
 
 
 @SpringBootApplication
+@Repository("com.github.eoinf.jiggen")
+@ImportResource({"classpath*:applicationContext.xml"})
 public class SpringifiedSparkFilter extends SparkFilter {
 
     @Override
     protected SparkApplication getApplication(String applicationClassName) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        return (SparkApplication) context.getBean(applicationClassName);
+        ApplicationContext context = SpringApplication.run(SpringifiedSparkFilter.class);
+        return (SparkApplication) context.getBean("Application");
     }
 }
