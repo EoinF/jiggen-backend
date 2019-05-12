526f5b0e5230d6fe999dac77245f23855aa45e9e
Eoin Flanagan <outterest@hotmail.com>
2019-05-12 12:07:54 +0200

    Moved chmod command from inside the docker buildtime script to the docker runtime script

--- ci/build-task.sh
+++ ci/build-task.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-set -x
+set -ex
 
 cd ..
+chmod +x gradlew
 ./gradlew war -q
 
 aws s3 cp build/libs/jiggen-backend.war s3://jiggen/jiggen-backend.war
--- ci/docker-builder/build.sh
+++ ci/docker-builder/build.sh
@@ -3,6 +3,5 @@ set -x
 
 git clone https://github.com/EoinF/jiggen-backend.git
 chmod +x jiggen-backend/ci/build-task.sh
-chmod +x jiggen-backend/gradlew
 cd jiggen-backend/ci
 ./build-task.sh
